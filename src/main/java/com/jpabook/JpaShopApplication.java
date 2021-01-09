8eec3ecdccb9caeb88ef8aacbd874e4dd466c0b7
kyhslam <kyhslam@gmail.com>
2021-01-09 13:39:07 +0900

    init

---
+++ src/main/java/com/jpabook/JpaShopApplication.java
@@ -0,0 +1,20 @@
+package com.jpabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaShopApplication {
+
+    public static void main(String[] args) {
+
+        Hello hello = new Hello();
+        hello.setData("hello");
+
+        String data = hello.getData();
+
+        System.out.println("data == " + data);
+
+        SpringApplication.run(JpaShopApplication.class, args);
+    }
+}
