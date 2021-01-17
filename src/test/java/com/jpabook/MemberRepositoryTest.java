b6cf7d229e0a20a01aa51b0dc989a9ee243ef5a3
kyhslam <kyhslam@gmail.com>
2021-01-17 17:25:13 +0900

    create member domain'

---
+++ src/test/java/com/jpabook/MemberRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.jpabook;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+
+@RunWith(SpringRunner.class) // 스프링관련된걸 테스트할 거야
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+
+    }
+
+}
