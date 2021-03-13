396ba74285b7e7326cbe130d0778fd5ef8957f2e
kyhslam <kyhslam@gmail.com>
2021-03-13 11:47:38 +0900

    html, css

---
+++ src/main/java/com/jpabook/repository/OrderSearch.java
@@ -0,0 +1,12 @@
+package com.jpabook.repository;
+
+import com.jpabook.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {
+
+    private String memberName; // 회원이름
+    private OrderStatus orderStatus; // 주문상태[ORDER, CANCEL]
+}
