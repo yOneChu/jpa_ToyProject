82a1469185280bae1cb2db88c35fc0e545fc1aa1
kyhslam <kyhslam@gmail.com>
2021-03-14 19:08:27 +0900

    회원 수정 API

--- src/main/java/com/jpabook/api/MemberApiController.java
+++ src/main/java/com/jpabook/api/MemberApiController.java
@@ -2,13 +2,13 @@ package com.jpabook.api;
 
 import com.jpabook.domain.Member;
 import com.jpabook.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 
 @RestController// = @Controller + @ResponseBody을 합친거다
 @RequiredArgsConstructor
@@ -45,8 +45,36 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    /**
+     * 수정 API
+     * @param id
+     * @param request
+     * @return
+     */
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+                                    @PathVariable("id") Long id,
+                                    @RequestBody @Valid UpdateMemberRequest request){
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName()); //  @AllArgsConstructor 있어서 이렇게 생성함
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
     @Data
     static class CreateMemberRequest {
+        @NotEmpty
         private String name;
     }
 
